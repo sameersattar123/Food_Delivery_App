8821cb2f7c32007d4168d4a826b02dcd8742566c
Sameer Sattar <109748943+sameersattar123@users.noreply.github.com>
2023-08-07 23:54:11 +0500

    third commit

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,8 +1,27 @@
+import Image from 'next/image'
 import React from 'react'
 
 const CartPage = () => {
   return (
-    <div>CartPage</div>
+    <div>
+      {/* PRODUCT PAGE */}
+      <div className="">
+        {/* SINGLE PAGE */}
+        <div className=" relative">
+          <Image src="/temporary/p1.png" alt="" fill className='object-contain' />
+          <div className="">
+            <h2>Silician</h2>
+            <span>Large</span>
+          </div>
+            <h2>$78.99</h2>
+            <span>X</span>
+        </div>
+      </div>
+      {/* PAYMENT PAGE */}
+      <div className="">
+
+      </div>
+    </div>
   )
 }
 
