6969b2ab4494d25b4effbe8b5c063ff6d0f6ef84
Sameer Sattar <109748943+sameersattar123@users.noreply.github.com>
2023-08-12 20:19:13 +0500

    second  commit

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -3,23 +3,58 @@ import React from 'react'
 
 const CartPage = () => {
   return (
-    <div>
+    <div className='  text-red-500 flex flex-col lg:flex-row'>
       {/* PRODUCT PAGE */}
-      <div className="">
+      <div className="flex-1 p-4 flex flex-col justify-center h-1/2 overflow-scrol lg:h-full lg:w-2/3 2xl:w-1/2 lg:px-20 xl:px-40">
         {/* SINGLE PAGE */}
-        <div className=" relative">
-          <Image src="/temporary/p1.png" alt="" fill className='object-contain' />
+        <div className="flex items-center mb-4 justify-between">
+          <Image src="/temporary/p1.png" alt="" width={100} height={100} />
           <div className="">
-            <h2>Silician</h2>
+            <h2 className='text-xl uppercase font-bold'>Silician</h2>
             <span>Large</span>
           </div>
-            <h2>$78.99</h2>
-            <span>X</span>
+            <h2 className='font-bold'>$78.99</h2>
+            <span className='cursor-ponter'>X</span>
         </div>
-      </div>
+        <div className="flex items-center mb-4 justify-between ">
+          <Image src="/temporary/p1.png" alt="" width={100} height={100} />
+          <div className="">
+            <h2 className='text-xl uppercase font-bold'>Silician</h2>
+            <span>Large</span>
+          </div>
+            <h2 className='font-bold'>$78.99</h2>
+            <span className='cursor-ponter'>X</span>
+        </div>
+        <div className="flex items-center mb-4 justify-between ">
+          <Image src="/temporary/p1.png" alt="" width={100} height={100} />
+          <div className="">
+            <h2 className='text-xl uppercase font-bold'>Silician</h2>
+            <span>Large</span>
+          </div>
+            <h2 className='font-bold'>$78.99</h2>
+            <span className='cursor-ponter'>X</span>
+        </div>
+        </div>
       {/* PAYMENT PAGE */}
-      <div className="">
-
+      <div className="flex-1 p-4 h-1/2 bg-fuchsia-50 flex flex-col gap-4 justify-center">
+        <div className="flex justify-between ">
+          <span className="">Subtotal (3)</span>
+          <span className="">$78.99</span>
+        </div>
+        <div className="flex justify-between ">
+          <span className="">Services cost</span>
+          <span className="">$0.00</span>
+        </div>
+        <div className="flex justify-between ">
+          <span className="">Delivery Cost</span>
+          <span className="text-green-500">FREE!</span>
+        </div>
+        <hr />
+        <div className="flex justify-between ">
+          <span className="">Total</span>
+          <span className="">$78.99</span>
+        </div>
+        <button className="bg-red-500 rounded-md w-1/2 p-3 text-white self-end">CHECKOUT</button>
       </div>
     </div>
   )
